ef4bafa7f852d16a89df4cf19b7727d07779974d
Jayromulus <jerbear2@gmail.com>
2024-12-02 01:46:43 -0500

    players can now swap, and go up in initiative order

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 const getId = id => document.getElementById(id)
 const log = console.log
+Array.prototype.swap = function (x, y) {
+  let b = this[x]
+  this[x] = this[y]
+  this[y] = b
+  return this
+}
 // const playerData = localStorage.getItem('players') ?? []
 
 const storeDummyData = getId('test-store')
 const clearDummyData = getId('clear-data')
 const readDummyData = getId('test-read')
+const furtherInitiative = getId('further-initiative')
+const reverseInitiative = getId('reverse-initiative')
 const display = getId('data-display')
 
+let playerLength = 0
+let initiativeIndex = 0
+
 window.onload = displayPlayerData();
+// track the "current turn" and highlight the player, starting from the top of initiative. probably have some sort of button to show that the combat will begin and highlight the player
 
 storeDummyData.addEventListener('click', e => {
   e.preventDefault()
@@ -38,63 +50,122 @@ readDummyData.addEventListener('click', e => {
   displayPlayerData()
 })
 
+furtherInitiative.addEventListener('click', e => {
+  e.preventDefault()
+  log('advancing initiative')
+  initiativeIndex < playerLength ? initiativeIndex++ : initiativeIndex = 0
+  displayPlayerData()
+})
+
+reverseInitiative.addEventListener('click', e => {
+  e.preventDefault()
+  log('reversing initiative')
+  initiativeIndex === 0 ? initiativeIndex = playerLength : initiativeIndex--
+  displayPlayerData()
+})
+
 function displayPlayerData() {
-  const currentPlayers = localStorage.getItem('players') ? JSON.parse(localStorage.getItem('players')) : 'No Player Information'
+  const currentPlayers = localStorage.getItem('players') ? JSON.parse(localStorage.getItem('players')) : []
   const newPlayerList = []
 
-  if(!currentPlayers) {
+  if(!currentPlayers[0]) {
     display.innerText = 'No Players Currently'
     return
   }
-  // temporarily gone to test displaying users visuals
-  // display.innerText = currentPlayers
+
+  playerLength = currentPlayers.length - 1
+  
   currentPlayers.forEach((player, index) => {
-    const newPlayer = document.createElement('div')
-    const newPlayerName = document.createElement('p')
-    const newPlayerHPNum = document.createElement('p')
-    const newPlayerHP = document.createElement('div')
-    const newPlayerCurrentHP = document.createElement('div')
-    const upInitiative = document.createElement('button')
-    const downInitiative = document.createElement('button')
-    
-    newPlayerName.innerText = player.name
-    newPlayerHPNum.innerText = `${player.currentHP} / ${player.maxHP}`
-    newPlayerCurrentHP.style.width = `${Math.floor((player.currentHP / player.maxHP)*100)}%`
-    upInitiative.innerText = '↑'
-    downInitiative.innerText = '↓'
-
-    upInitiative.addEventListener('click', e => {
-      e.preventDefault()
-      increaseInitiative(currentPlayers, index)
-    })
-    downInitiative.addEventListener('click', e => {
-      e.preventDefault()
-      decreaseInitiative(currentPlayers, index)
-    })
-    
-    newPlayer.classList.add('player')
-    newPlayerName.classList.add('player-name')
-    newPlayerHPNum.classList.add('player-hp-num')
-    newPlayerHP.classList.add('player-hp')
-    
-    newPlayer.appendChild(newPlayerName)
-    newPlayer.appendChild(newPlayerHPNum)
-    newPlayer.appendChild(upInitiative)
-    newPlayer.appendChild(downInitiative)
-    newPlayerHP.appendChild(newPlayerCurrentHP)
-    newPlayer.appendChild(newPlayerHP)
-
+    const newPlayer = generatePlayerCard(player, index, currentPlayers)
     newPlayerList.push(newPlayer)
   });
+
+  log({currentPlayers})
 
   display.innerHTML = ''
   display.append(...newPlayerList)
 }
 
+function generatePlayerCard(player, index, currentPlayers) {
+  const newPlayer = document.createElement('div')
+  const newPlayerName = document.createElement('p')
+  const newPlayerHPNum = document.createElement('p')
+  const newPlayerHP = document.createElement('div')
+  const newPlayerCurrentHP = document.createElement('div')
+  const upInitiative = document.createElement('button')
+  const downInitiative = document.createElement('button')
+  const healthChange = document.createElement('input')
+  const addHealthBtn = document.createElement('button')
+  const loseHealthBtn = document.createElement('button')
+  
+  newPlayerName.innerText = player.name
+  newPlayerHP.style.backgroundColor = index === initiativeIndex ? 'red' : 'black'
+  newPlayerHPNum.innerText = `${player.currentHP} / ${player.maxHP}`
+  newPlayerCurrentHP.style.width = `${Math.floor((player.currentHP / player.maxHP)*100)}%`
+  upInitiative.innerText = '↑'
+  downInitiative.innerText = '↓'
+  addHealthBtn.innerText = '+'
+  loseHealthBtn.innerText = '-'
+  healthChange.type = 'number'
+
+  upInitiative.addEventListener('click', e => {
+    e.preventDefault()
+    increaseInitiative(currentPlayers, index)
+  })
+  downInitiative.addEventListener('click', e => {
+    e.preventDefault()
+    decreaseInitiative(currentPlayers, index)
+  })
+  addHealthBtn.addEventListener('click', e => {
+    e.preventDefault()
+    addHealth(currentPlayers, index, healthChange.value)
+    healthChange.value = null
+  })
+  loseHealthBtn.addEventListener('click', e => {
+    e.preventDefault()
+    loseHealth(currentPlayers, index, healthChange.value)
+    healthChange.value = null
+  })
+  
+  newPlayer.classList.add('player')
+  newPlayerName.classList.add('player-name')
+  newPlayerHPNum.classList.add('player-hp-num')
+  newPlayerHP.classList.add('player-hp')
+  
+  newPlayer.appendChild(newPlayerName)
+  newPlayer.appendChild(newPlayerHPNum)
+  newPlayer.appendChild(upInitiative)
+  newPlayer.appendChild(downInitiative)
+  newPlayer.appendChild(healthChange)
+  newPlayer.appendChild(addHealthBtn)
+  newPlayer.appendChild(loseHealthBtn)
+  newPlayerHP.appendChild(newPlayerCurrentHP)
+  newPlayer.appendChild(newPlayerHP)
+
+  return newPlayer
+}
+
 function increaseInitiative(players, current) {
   log(`move the player [${players[current].name}] up in initiative by 1, then re-render`)
+  log({players})
+  target = current > 0 ? current-- : players.length - 1
+  players.swap(current, target)
+  log({players})
+  localStorage.setItem('players', JSON.stringify(players))
+  initiativeIndex === 0 ? initiativeIndex = playerLength : initiativeIndex--
+  displayPlayerData()
 }
 
 function decreaseInitiative(players, current) {
   log(`move the player [${players[current].name}] down in initiative by 1, then re-render`)
+  
+}
+
+function addHealth(players, current, amount) {
+  log(`add ${amount} health to ${players[current].currentHP}, then re-render`)
+
+}
+
+function loseHealth(players, current, amount) {
+  log(`remove ${amount} health from ${players[current].currentHP}, minumum 1, then re-render. if the health goes to zero, move them above the person who placed them at 0 health as tracked by the "current turn"`)
 }
---
+++ test.js
@@ -0,0 +1,4 @@
+const testArr = [1, 2, 3, 4]
+console.log(testArr)
+[testArr[0], testArr[1]] = [testArr[1], testArr[0]]
+console.log(testArr)
