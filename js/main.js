743ec70c969e1f031ebc87c8f312125dfb96c12c
Jayromulus <jerbear2@gmail.com>
2024-12-02 00:42:13 -0500

    adding dummy data to webpage

--- js/main.js
+++ js/main.js
@@ -1,22 +1,74 @@
-const getId = document.getElementById
+const getId = id => document.getElementById(id)
 const log = console.log
+// const playerData = localStorage.getItem('players') ?? []
 
-const storeData = getId('test-store')
-const rewriteData = getId('test-rewrite')
-const readData = getId('test-read')
+const storeDummyData = getId('test-store')
+const clearDummyData = getId('clear-data')
+const readDummyData = getId('test-read')
 const display = getId('data-display')
 
-storeData.addEventListener(e => {
+window.onload = displayPlayerData();
+
+storeDummyData.addEventListener('click', e => {
   e.preventDefault()
   log('storing data')
+
+  const currentPlayers = localStorage.getItem('players') ? JSON.parse(localStorage.getItem('players')) : []
+
+  log({currentPlayers})
+
+  const maxHP = Math.floor(Math.random()*100)+1
+  currentPlayers.push({name: `Player ${Math.random().toString().slice(-4)}`, maxHP, currentHP: Math.floor(Math.random()*maxHP)+1 })
+
+  localStorage.setItem('players', JSON.stringify(currentPlayers))
+
+  displayPlayerData()
 })
 
-rewriteData.addEventListener(e => {
+clearDummyData.addEventListener('click', e => {
   e.preventDefault()
-  log('rewriting data')
+  log('clearing data')
+  localStorage.setItem('players', '')
+  displayPlayerData()
 })
 
-readData.addEventListener(e => {
+readDummyData.addEventListener('click', e => {
   e.preventDefault()
   log('reading data')
+  displayPlayerData()
 })
+
+function displayPlayerData() {
+  const currentPlayers = localStorage.getItem('players') ? JSON.parse(localStorage.getItem('players')) : 'No Player Information'
+  const newPlayerList = []
+
+  if(!currentPlayers) {
+    display.innerText = 'No Players Currently'
+    return
+  }
+  // temporarily gone to test displaying users visuals
+  // display.innerText = currentPlayers
+  currentPlayers.forEach(player => {
+    const newPlayer = document.createElement('div')
+    const newPlayerName = document.createElement('p')
+    const newPlayerHPNum = document.createElement('p')
+    const newPlayerHP = document.createElement('div')
+    const newPlayerCurrentHP = document.createElement('div')
+    
+    newPlayer.innerText = player.name
+    newPlayerHPNum.innerText = `${player.currentHP} / ${player.maxHP}`
+    newPlayerCurrentHP.style.width = `${Math.floor((player.currentHP / player.maxHP)*100)}%`
+    
+    newPlayerName.classList.add('player-name')
+    newPlayerHP.classList.add('player-hp')
+    
+    newPlayer.appendChild(newPlayerName)
+    newPlayerHP.appendChild(newPlayerCurrentHP)
+    newPlayer.appendChild(newPlayerHP)
+
+    newPlayerList.push(newPlayer)
+  });
+
+  display.innerHTML = ''
+  display.append(...newPlayerList)
+}
