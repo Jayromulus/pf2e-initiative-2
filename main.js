a4a03b9ddac6994c3bdfb035728caeeae4426cfe
Jayromulus <jerbear2@gmail.com>
2025-04-05 00:26:50 -0400

    saving initaitive between sessions

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const getId = id => document.getElementById(id)
-const log = console.log
+const getId = id => document.getElementById(id);
+const log = console.log;
 Array.prototype.swap = function (x, y) {
   let b = this[x]
   this[x] = this[y]
@@ -8,23 +8,24 @@ Array.prototype.swap = function (x, y) {
 }
 // const playerData = localStorage.getItem('players') ?? []
 
-const storeDummyData = getId('test-store')
-const clearDummyData = getId('clear-data')
-const furtherInitiative = getId('further-initiative')
-const reverseInitiative = getId('reverse-initiative')
-const initiativeDisplay = getId('initiative')
-const display = getId('data-display')
-const dialog = document.querySelector('dialog');
+const storeDummyData = getId('test-store');
+const clearDummyData = getId('clear-data');
+const furtherInitiative = getId('further-initiative');
+const reverseInitiative = getId('reverse-initiative');
+const initiativeDisplay = getId('initiative');
+const display = getId('data-display');
+const dialog = document.getElementById('add-character-dialog');
 const closeDialog = document.getElementById('close-dialog');
 const addCharacter = document.getElementById('add-player');
 const newCharacterName = document.getElementById('character-name');
 const newCharacterHP = document.getElementById('character-hp');
 
-let playerLength = 0
+// let playerLength = 0;
 // move initiativeIndex to localstorage so that it retains between sessions as needed
 // with a button to reset position to top as well as button to reset board but keeping party
 // maybe with a tag on players to indicate they are party
-let initiativeIndex = 0
+let initiativeIndex = localStorage.getItem('initiativeIndex') ? Number(localStorage.getItem('initiativeIndex')) : 0;
+log(typeof localStorage.getItem('initiativeIndex'));
 
 window.onload = displayPlayerData();
 // track the "current turn" and highlight the player, starting from the top of initiative.
@@ -80,6 +81,7 @@ furtherInitiative.addEventListener('mousedown', e => {
   e.preventDefault()
   log('advancing initiative')
   initiativeIndex < playerLength ? initiativeIndex++ : initiativeIndex = 0
+	localStorage.setItem('initiativeIndex', initiativeIndex);
   displayPlayerData()
 })
 
@@ -87,6 +89,7 @@ reverseInitiative.addEventListener('mousedown', e => {
   e.preventDefault()
   log('reversing initiative')
   initiativeIndex === 0 ? initiativeIndex = playerLength : initiativeIndex--
+	localStorage.setItem('initiativeIndex', initiativeIndex);
   displayPlayerData()
 })
 
@@ -109,71 +112,71 @@ function displayPlayerData() {
   // log({currentPlayers})
 
   display.innerHTML = ''
-  initiativeDisplay.innerText = initiativeIndex
+  // initiativeDisplay.innerText = initiativeIndex
   display.append(...newPlayerList)
 }
 
 function generatePlayerCard(player, index, currentPlayers) {
-  const newPlayer = document.createElement('div')
-  const newPlayerName = document.createElement('p')
-  const newPlayerHPNum = document.createElement('p')
-  const newPlayerHP = document.createElement('div')
-  const newPlayerCurrentHP = document.createElement('div')
-  const upInitiative = document.createElement('button')
-  const downInitiative = document.createElement('button')
-  const healthChange = document.createElement('input')
-  const addHealthBtn = document.createElement('button')
-  const loseHealthBtn = document.createElement('button')
+  const newPlayer = document.createElement('div');
+  const newPlayerName = document.createElement('p');
+  const newPlayerHPNum = document.createElement('p');
+  const newPlayerHP = document.createElement('div');
+  const newPlayerCurrentHP = document.createElement('div');
+  const upInitiative = document.createElement('button');
+  const downInitiative = document.createElement('button');
+  const healthChange = document.createElement('input');
+  const addHealthBtn = document.createElement('button');
+  const loseHealthBtn = document.createElement('button');
   
-  newPlayerName.innerText = player.name
-  newPlayerHP.style.backgroundColor = index === initiativeIndex ? 'red' : 'black'
-  newPlayerHPNum.innerText = `${player.currentHP} / ${player.maxHP}`
-  newPlayerCurrentHP.style.width = `${Math.floor((player.currentHP / player.maxHP)*100)}%`
-  upInitiative.innerText = '↑'
-  downInitiative.innerText = '↓'
-  addHealthBtn.innerText = '+'
-  loseHealthBtn.innerText = '-'
-  healthChange.type = 'number'
+  newPlayerName.innerText = player.name;
+  newPlayerHP.style.backgroundColor = index === initiativeIndex ? 'red' : 'black';
+  newPlayerHPNum.innerText = `${player.currentHP} / ${player.maxHP}`;
+  newPlayerCurrentHP.style.width = `${Math.floor((player.currentHP / player.maxHP)*100)}%`;
+  upInitiative.innerText = '↑';
+  downInitiative.innerText = '↓';
+  addHealthBtn.innerText = '+';
+  loseHealthBtn.innerText = '-';
+  healthChange.type = 'number';
 
   upInitiative.addEventListener('mousedown', e => {
-    e.preventDefault()
-    increaseInitiativeOrder(currentPlayers, index)
+    e.preventDefault();
+    increaseInitiativeOrder(currentPlayers, index);
   })
   downInitiative.addEventListener('mousedown', e => {
-    e.preventDefault()
-    decreaseInitiativeOrder(currentPlayers, index)
+    e.preventDefault();
+    decreaseInitiativeOrder(currentPlayers, index);
   })
   addHealthBtn.addEventListener('mousedown', e => {
-    e.preventDefault()
-    addHealth(currentPlayers, index, healthChange.value)
-    healthChange.value = null
+    e.preventDefault();
+    addHealth(currentPlayers, index, healthChange.value);
+    healthChange.value = null;
   })
   loseHealthBtn.addEventListener('mousedown', e => {
-    e.preventDefault()
-    loseHealth(currentPlayers, index, healthChange.value)
-    healthChange.value = null
+    e.preventDefault();
+    loseHealth(currentPlayers, index, healthChange.value);
+    healthChange.value = null;
   })
   
-  newPlayer.classList.add('player')
-  newPlayerName.classList.add('player-name')
-  newPlayerHPNum.classList.add('player-hp-num')
-  newPlayerHP.classList.add('player-hp')
+  newPlayer.classList.add('player');
+  newPlayerName.classList.add('player-name');
+  newPlayerHPNum.classList.add('player-hp-num');
+  newPlayerHP.classList.add('player-hp');
   
-  newPlayer.appendChild(newPlayerName)
-  newPlayer.appendChild(newPlayerHPNum)
-  newPlayer.appendChild(upInitiative)
-  newPlayer.appendChild(downInitiative)
-  newPlayer.appendChild(healthChange)
-  newPlayer.appendChild(addHealthBtn)
-  newPlayer.appendChild(loseHealthBtn)
-  newPlayerHP.appendChild(newPlayerCurrentHP)
-  newPlayer.appendChild(newPlayerHP)
+  newPlayer.appendChild(newPlayerName);
+  newPlayer.appendChild(newPlayerHPNum);
+  newPlayer.appendChild(upInitiative);
+  newPlayer.appendChild(downInitiative);
+  newPlayer.appendChild(healthChange);
+  newPlayer.appendChild(addHealthBtn);
+  newPlayer.appendChild(loseHealthBtn);
+  newPlayerHP.appendChild(newPlayerCurrentHP);
+  newPlayer.appendChild(newPlayerHP);
 
-  return newPlayer
+  return newPlayer;
 }
 
 function increaseInitiativeOrder(players, current) {
-  target = current > 0 ? current-- : players.length - 1
+  target = current > 0 ? current-- : players.length - 1;
 
   initiativeIndex === 0 && current === initiativeIndex && target === playerLength ? 
     initiativeIndex = playerLength :
@@ -185,13 +188,15 @@ function increaseInitiativeOrder(players, current) {
     initiativeIndex++ :
   null
 
-  players.swap(current, target)
+  players.swap(current, target);
 
-  savePlayers(players)
+	localStorage.setItem('initiativeIndex', initiativeIndex);
+
+  savePlayers(players);
 }
 
 function decreaseInitiativeOrder(players, current) {
-  target = current < playerLength ? current++ : 0
+  target = current < playerLength ? current++ : 0;
 
   initiativeIndex+1 > playerLength && current === initiativeIndex && target < 1 ?
     initiativeIndex = 0 :
@@ -203,56 +208,58 @@ function decreaseInitiativeOrder(players, current) {
     initiativeIndex-- :
   null
   
-  players.swap(current, target)
+  players.swap(current, target);
+
+	localStorage.setItem('initiativeIndex', initiativeIndex);
   
-  savePlayers(players)
+  savePlayers(players);
 }
 
 function addHealth(players, current, amount) {
-  log(`add ${amount} health to ${players[current].currentHP}, then re-render`)
+  log(`add ${amount} health to ${players[current].currentHP}, then re-render`);
   // overheal bar should just be an optional extra bar nested within the hp bar,
   // similar styling but yellow rather than green
-  max = players[current].maxHP
-  cur = players[current].currentHP
-  amount = Number(amount)
-  let newHealth = cur + amount >= max ? max : cur + amount
+  max = players[current].maxHP;
+  cur = players[current].currentHP;
+  amount = Number(amount);
+  let newHealth = cur + amount >= max ? max : cur + amount;
 
-  log({newHealth})
+  log({newHealth});
 
-  players[current].currentHP = newHealth
-  savePlayers(players)
+  players[current].currentHP = newHealth;
+  savePlayers(players);
 }
 
 function loseHealth(players, current, amount) {
-  log(`remove ${amount} health from ${players[current].currentHP}, minumum 1, then re-render. if the health goes to zero, move them above the person who placed them at 0 health as tracked by the "current turn"`)
-  max = players[current].maxHP
-  cur = players[current].currentHP
-  amount = Number(amount)
-  let newHealth = cur - amount < 0 ? 0 : cur - amount
+  log(`remove ${amount} health from ${players[current].currentHP}, minumum 1, then re-render. if the health goes to zero, move them above the person who placed them at 0 health as tracked by the "current turn"`);
+  max = players[current].maxHP;
+  cur = players[current].currentHP;
+  amount = Number(amount);
+  let newHealth = cur - amount < 0 ? 0 : cur - amount;
 
-  log({newHealth})
+  log({newHealth});
 
-  players[current].currentHP = newHealth
+  players[current].currentHP = newHealth;
 
   if(newHealth === 0)
-    movePlayerPosition(players, current, initiativeIndex)
-  savePlayers(players)
+    movePlayerPosition(players, current, initiativeIndex);
+  savePlayers(players);
 }
 
 function movePlayerPosition(players, current, goal) {
   if(goal >= players.length) {
-    log('failed to move')
-    return
+    log('failed to move');
+    return;
   }
   // let i = 0
   // while(i < 2) {
   //   increaseInitiativeOrder(players, current)
   //   i++
   // }
-  log('repeat the swap command, until the current player is above the initiativeIndex position, making space if needed at top of initiative')
+  log('repeat the swap command, until the current player is above the initiativeIndex position, making space if needed at top of initiative');
 }
 
 function savePlayers(players) {
   localStorage.setItem('players', JSON.stringify(players));
-  displayPlayerData()
+  displayPlayerData();
 }
